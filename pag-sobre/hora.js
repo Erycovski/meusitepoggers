ce5ad0eb0391ddea92aee2b96f293853eacd3843
Erycovsk <98654999+Erycovski@users.noreply.github.com>
2022-03-25 01:19:32 -0300

    viking pag bot
    pag pra mostrar os comandos do meu projeto viking bot ( esta privado)

--- pag-sobre/hora.js
+++ pag-sobre/hora.js
@@ -1,8 +1,3 @@
-agora = new Date()
-var hora = agora.getHours()
-var minuto = agora.getMinutes()
-var segundo = agora.getSeconds()
-
 if (hora < 12) {
     saudacao.innerHTML = "Bom dia!"
 } else if (hora <= 18) {
@@ -11,12 +6,33 @@ if (hora < 12) {
     saudacao.innerHTML = "Boa Noite!"
 } 
 
-var horacerta = document.getElementById('horacertabox')
-var horacertatext = "Agora são exatamente: " +  hora + ":" + minuto + ":" + segundo + "."
-horacerta.innerHTML = horacertatext
+
+function recarregarPagina() {
+    var agora = new Date()
+    var hora = agora.getHours()
+    var minuto = agora.getMinutes()
+    var segundo = agora.getSeconds()
+    
+    if (hora < 12) {
+        saudacao.innerHTML = "Bom dia!"
+    } else if (hora <= 18) {
+        saudacao.innerHTML = "Boa Tarde!"
+    } else {
+        saudacao.innerHTML = "Boa Noite!"
+    }
+
+    
+
+
+    var horacerta = document.getElementById('horacertabox')
+    var horacertatext = "Agora são exatamente: " +  hora + ":" + minuto + ":" + segundo + "."
+    horacerta.innerHTML = horacertatext
 
 
 
-  
 
+    setInterval( function() {recarregarPagina(); } , 1000*60)
 
+}
+
+recarregarPagina()
